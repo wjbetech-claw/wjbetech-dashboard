d057d5e0f0f768b11249684a34da4f831bd9595e
wjbetech <wjbetech@gmail.com>
2026-02-23 22:30:47 +0900

    fixing vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
 
-export default defineConfig({
-  plugins: [react()],
-  server: { host: true }
-})
+export default defineConfig(async () => {
+  const react = (await import("@vitejs/plugin-react")).default;
+  return {
+    plugins: [react()],
+    server: { host: true }
+  };
+});
