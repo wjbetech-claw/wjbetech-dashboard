67e6e2cafb4a467b16af4f1a50c25a68f4cd473f
wjbetech <wjbetech@gmail.com>
2026-02-25 18:47:10 +0900

    chore: fixing broken openslop

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
-<<<<<<< Updated upstream
-import './styles/ui-enhance.css'
-=======
-import './styles/tokens.generated.css'
->>>>>>> Stashed changes
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import { BrowserRouter } from 'react-router-dom'
-import './styles/tokens.css'
+import "./styles/tokens.generated.css";
+import "./styles/tokens.css";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(<React.StrictMode><BrowserRouter><App/></BrowserRouter></React.StrictMode>)
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
