d297d98c113d76c38c8fae7963cc45840633db80
wjbetech-claw <wjbeclaw@gmail.com>
2026-02-28 18:48:01 +0900

    chore: sync workspace changes (#123)
    Co-authored-by: openclaw-bot <bot@openclaw.local>

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import MainDashboard from './MainDashboard'
 
+import EmptyState from '../components/EmptyState'
+
 export default function DashboardPage(){
   return <MainDashboard />
 }
